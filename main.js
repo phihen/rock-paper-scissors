b702a0a8da3288db8dd43e30328130fe7a54b795
phihen <48790552+phihen@users.noreply.github.com>
2025-03-30 17:15:37 +0200

    Add logic for human choice

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function getComputerChoice() {
             break;
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("0 - rock\n1 - paper\n2 - scissors\nPlease insert your choice:");
+    switch (humanChoice) {
+        case "0":
+            return "rock";
+            break;
+        case "1":
+            return "paper";
+            break;
+        case "2":
+            return "scissors";
+            break;
+    }
+}
